9d1db65ab50b5c49dd72f35f0d8a3e0039358f0f
wbenbihi <waligoo@gmail.com>
2023-03-02 21:48:33 +0100

    [DOC](examples) Add comments in `base` example

--- examples/base.py
+++ examples/base.py
@@ -5,20 +5,29 @@ from fastapi import Depends
 from fastapi import FastAPI
 from fastapi import HTTPException
 from pydantic import BaseModel
-from fastapi.responses import JSONResponse
 
 import fastjwt
 from fastjwt import FastJWT
 from fastjwt import FastJWTConfig
-from fastjwt.payload import JWTPayload
+
+# ================================================================
+# MOCKUP DATABASE
+# ================================================================
+# Here we simulate a Database containing 2 registerd users
 
 
+# We define a User model as a `TypedDict`
+#   Note that the user model can be any type of object you want
+#   and does not need to be a BaseModel or TypedDict
 class User(TypedDict):
     name: str
     email: str
     password: str
 
 
+# We populate a fake user database as a python dictionary
+#   Keys represent User ID (here user's email)
+#   Value represent the User Object model to be returned
 DB: Dict[str, User] = {
     "john.doe@test.com": {
         "name": "John Doe",
@@ -32,7 +41,26 @@ DB: Dict[str, User] = {
     },
 }
 
+# ================================================================
+# APPLICATION
+# ================================================================
+# We define the FastAPI as usual
+app = FastAPI(title="Base FastJWT Example", version=fastjwt.__version__, debug=True)
 
+# We then generate the configuration regarding the desired behavior or the JWT Plugin
+config = FastJWTConfig()
+config.JWT_COOKIE_SECURE = False  # Secure must always be set to True in Production
+config.JWT_ALGORITHM = "HS256"  # We use a symmetric algorithm for the example, please consider using asymmetric algorithm for better protection
+config.JWT_SECRET_KEY = "Secret Key"  # We set the string to be used as a secret key
+config.JWT_LOCATIONS = [
+    "headers",
+    "query",
+    "json",
+]  # We configure the plugin to only look for JWT in Headers, Query Parameters or JSON Body. We removed token in cookies
+
+
+# We define an accessor `(str) -> User` that given a unique user identifier
+# returns an instance of the User Model Object
 def get_user_from_db(uid: str) -> User:
     """Simulate fetching a User from a database
 
@@ -45,14 +73,9 @@ def get_user_from_db(uid: str) -> User:
     return DB.get(uid)
 
 
-app = FastAPI(title="Base FastJWT Example", version=fastjwt.__version__, debug=True)
-
-config = FastJWTConfig()
-config.JWT_COOKIE_SECURE = False
-config.JWT_ALGORITHM = "HS256"
-config.JWT_SECRET_KEY = "SFVDSDVD"
-
-security: FastJWT = FastJWT(user_model=User, payload_model=JWTPayload, config=config)
+# Once your configuration is done you can instantiate the FastJWT object
+security: FastJWT = FastJWT(user_model=User, config=config)
+# We set our custom callback to the FastJWT object
 security.set_user_getter(get_user_from_db)
 
 
@@ -68,26 +91,37 @@ def home():
 
 @app.post("/login")
 def login(data: LoginForm):
+    # FastJWT is not a validation tool or database management library
+    # Therefore you'll need to implement the login/logout logic on
+    # your own. The code below is a dummy example for the login logic
+
+    # Check if the data is available in the request
     if (not data.email) or (not data.password):
         raise HTTPException(400, detail={"message": "BAD REQUEST"})
+
     email = data.email
     password = data.password
+
+    # Check if the user exists in the database
     if email not in DB:
         raise HTTPException(401, detail={"message": f"User {email} does not exists"})
-    indexed_user = DB[email]
-    if email == indexed_user["email"] and password == indexed_user["password"]:
+
+    # Check if the email/password tuple is valid
+    if email == DB[email]["email"] and password == DB[email]["password"]:
+        # Once all check are done.
+        # Generate an access token with the `FastJWT.create_access_token` method
         token = security.create_access_token(uid=email)
-        response = JSONResponse(content={"access_token": token})
-        security.set_access_cookie(response=response, token=token)
-        return response
+        return {"access_token": token}
     raise HTTPException(401, detail={"message": "Bad credentials"})
 
 
-@app.get("/me")
-def profile(user: User = Depends(security.get_current_user_callback)):
-    return f"First Name: {user['name'].split(' ')[0]} | Last Name: {user['name'].split(' ')[1]}"
-
-
+# Use the FastJWT.auth_required Dependency as a route argument to protected a route
 @app.get("/protected", dependencies=[Depends(security.auth_required)])
 def protected():
     return "You have access to this protected route"
+
+
+# Use the FastJWT.get_current_user_callback Dependency as a function argument to retrieve the User object
+@app.get("/me")
+def profile(user: User = Depends(security.get_current_user_callback)):
+    return f"First Name: {user['name'].split(' ')[0]} | Last Name: {user['name'].split(' ')[1]}"
