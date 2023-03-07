284f74cca0ad6a83eef9a53e7a70ab56c394e449
wbenbihi <waligoo@gmail.com>
2023-03-07 20:32:01 +0100

    [RM] Old quick imports

--- fastjwt/__init__.py
+++ fastjwt/__init__.py
@@ -1,6 +1,1 @@
-from fastjwt.types import RequestToken
-from fastjwt.fastjwt import FastJWT
-from fastjwt.settings import FastJWTConfig
-from fastjwt.settings import FastJWTConfigDict
-
 __version__ = "0.0.0"
