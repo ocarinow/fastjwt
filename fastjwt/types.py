6890ddc2110bf0263206928f2ee0b4c008ccd640
wbenbihi <waligoo@gmail.com>
2023-03-07 20:29:37 +0100

    [ADD] type hints

--- fastjwt/types.py
+++ fastjwt/types.py
@@ -1,17 +1,48 @@
-from typing import List
+import datetime
+from typing import Union
+from typing import Generic
 from typing import Literal
+from typing import TypeVar
+from typing import Callable
+from typing import Iterable
 from typing import Optional
+from typing import Awaitable
+from typing import ParamSpecKwargs
 
-from pydantic import BaseModel
+# Helper types
+T = TypeVar("T")
+Numeric = Union[float, int]
+ObjOrIter = Union[T, Iterable[T]]
+StrOrIter = ObjOrIter[str]
 
-AlgorithmType = Literal["RS256", "HS256"]
-SamesiteType = Literal["lax", "strict", "none"]
-TokenLocation = Literal["json", "headers", "cookies", "query"]
-TokenLocations = List[TokenLocation]
 
+# Datetime
+DateTimeExpression = Union[datetime.datetime, datetime.timedelta]
 
-class RequestToken(BaseModel):
-    access_token: Optional[str] = None
-    refresh_token: Optional[str] = None
-    csrf_token: Optional[str] = None
-    location: TokenLocation
+# Algorithm
+SymmetricAlgorithmType = Literal[
+    "HS256",
+    "HS384",
+    "HS512",
+]
+AsymmetricAlgorithmType = Literal[
+    "ES256",
+    "ES256K",
+    "ES384",
+    "ES512",
+    "RS256",
+    "RS384",
+    "RS512",
+    "PS256",
+    "PS384",
+    "PS512",
+]
+AlgorithmType = Union[SymmetricAlgorithmType, AsymmetricAlgorithmType]
+
+# Literal Enums
+HTTPMethod = Literal["GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]
+HTTPMethods = Iterable[HTTPMethod]
+SameSitePolicy = Literal["None", "Lax", "Strict"]
+TokenType = Literal["access", "refresh"]
+TokenLocation = Literal["headers", "cookies", "json", "query"]
+TokenLocations = Iterable[TokenLocation]
