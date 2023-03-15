96e10ec96fffd6d1d78847d312a544fc2efbf5a2
wbenbihi <waligoo@gmail.com>
2023-03-15 03:16:15 +0100

    [FIX][CI] Force virtualenv in project for Release workflow

--- ci/pre_commit_command.sh
+++ ci/pre_commit_command.sh
@@ -6,11 +6,12 @@ python3 -m venv $POETRY_HOME
 $POETRY_HOME/bin/pip install poetry==1.4.0
 $POETRY_HOME/bin/poetry --version
 # Install dependencies
-poetry install --no-interaction --no-root --with dev
+$POETRY_HOME/bin/poetry config virtualenvs.in-project true 
+$POETRY_HOME/bin/poetry install --no-interaction --no-root --with dev
 # Run Commands
-poetry run pytest tests/
-poetry run flake8 fastjwt --statistics --tee --output-file ./reports/flake8stats.txt
-poetry run genbadge tests -i reports/junit.xml -o - > reports/tests-badge.svg
-poetry run genbadge coverage -i reports/coverage.xml -o - > reports/coverage-badge.svg
-poetry run genbadge flake8 -i reports/flake8stats.txt -o - > reports/flake8-badge.svg
-poetry run docstr-coverage
+$POETRY_HOME/bin/poetry run pytest tests/
+$POETRY_HOME/bin/poetry run flake8 fastjwt --statistics --tee --output-file ./reports/flake8stats.txt
+$POETRY_HOME/bin/poetry run genbadge tests -i reports/junit.xml -o - > reports/tests-badge.svg
+$POETRY_HOME/bin/poetry run genbadge coverage -i reports/coverage.xml -o - > reports/coverage-badge.svg
+$POETRY_HOME/bin/poetry run genbadge flake8 -i reports/flake8stats.txt -o - > reports/flake8-badge.svg
+$POETRY_HOME/bin/poetry run docstr-coverage
