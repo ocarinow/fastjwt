0591e80e93373ed4d4194b249d092c7338875649
wbenbihi <waligoo@gmail.com>
2023-03-15 03:14:29 +0100

    [FIX][CI] Force poetry install on semantic release task

--- ci/pre_commit_command.sh
+++ ci/pre_commit_command.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Install poetry
+export POETRY_HOME=/opt/poetry
+python3 -m venv $POETRY_HOME
+$POETRY_HOME/bin/pip install poetry==1.4.0
+$POETRY_HOME/bin/poetry --version
+# Install dependencies
+poetry install --no-interaction --no-root --with dev
+# Run Commands
 poetry run pytest tests/
 poetry run flake8 fastjwt --statistics --tee --output-file ./reports/flake8stats.txt
 poetry run genbadge tests -i reports/junit.xml -o - > reports/tests-badge.svg
